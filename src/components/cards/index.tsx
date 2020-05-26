afe55a5e10bbfcf398bdb732fddb739d3ff1637e
evandrotvc <evandro-tvc@hotmail.com>
2020-05-26 11:20:58 -0300

    Bug filter removed.

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -1,17 +1,21 @@
-import React , {useState , useEffect} from 'react'
+import React , {useState , useEffect } from 'react'
+import { useImmer} from 'use-immer'
+import {produce} from 'immer'
+import {uuid} from 'uuidv4'
 import {FaRegTrashAlt} from 'react-icons/fa'
 import {Card,Form , Repository} from './styles'
 
 interface ToDos{
+    id: string;
     todos: string;
-    box: false;
+    completed: boolean;
 }
 const Dashboard: React.FC = () => {
     const [newtodos , setNewtodos] = useState<string >('')
-    const [checked , setChecked] = useState<string[]>([])
-    const [repository , setRepository] = useState<ToDos[]>([])
 
-    const [todos , setTodos] = useState <string[]>( () =>
+    const [Filter , setFilter] =  useState<string>('all');
+
+    const [todos , setTodos] = useImmer <ToDos[]>( () =>
     {
         const getTodos = localStorage.getItem('@React:todos')
 
@@ -33,54 +37,66 @@ const Dashboard: React.FC = () => {
         }
         const teste : string = newtodos
 
-        setTodos([...todos , teste ])
-        console.log(todos)
+        setTodos( todos => {todos.push({id: uuid(),todos : teste ,completed : false })} )
         setNewtodos('')
     }
 
-    function check(){
-        var box: any = document.getElementsByName("item");
-        const redes_user : string[] = []
+    function click(id: string , index : number) {
 
-        for (var i=0;i<box.length;i++){
-            if (box[i].checked === true){
-                // CheckBox Marcado...
-                redes_user.push(box[i].value) // vetor das redes
-                //alert(box[i].value + " marcado.");
+        setTodos( todos   => { // mudar um campo utilizando o id = "string"
+                todos.forEach((elem , i) =>{
+                    if(todos[i].id === id){
+                     todos[i].completed = !todos[i].completed
+                }
+            })
+        })
 
+     }
+
+    function HandleRemoveToDo(index : number , id : string) : void{
+
+        // setTodos(todos => {todos.splice(index , 1)}); remover quando tem o indice pronto
+        // setTodos(() => todos.filter(item => item.id !== id));
+
+        setTodos( todos   => { // remover elemento usando  id q é uma string
+            todos.forEach((elem , i) =>{
+                if(todos[i].id === id){
+                    setTodos(todos => {todos.splice(i , 1)});
             }
-        }
-        setChecked(redes_user)
-        console.log(checked)
-    }
+        })
+    })
 
-
-    function HandleRemoveToDo(index : number ) : void{
-        setTodos(todos.filter((item, ind) => index !== ind));
     }
 
     return (
     <>
+
         <Card>
         <Form onSubmit={ handleAddTodos}>
             <input value = {newtodos} placeholder= "add todos" onChange= {(e) => setNewtodos(e.target.value)}  />
 
         </Form>
 
-        {todos.map((todo , index) => (
-            <Repository key={index}>
-
-            <input name= "item" type= "checkbox" onChange = {check} value = {index}></input>
-            <label>{todo}</label>
-
-
-            <FaRegTrashAlt onClick= {() => HandleRemoveToDo(index)}/>
-        </Repository>
+        {todos
+        .filter((elem ) => {
+           if(Filter === "all" ) return true
+           if(Filter === "completed" ) return elem.completed
+           if(Filter === "pending" ) return !elem.completed
+           return false;
+        })
+        .map((todo , index) => (
+            <Repository key={todo.id} >
+                <input name= "item" type= "checkbox" onClick = {(e) => click(todo.id , index)} checked = {todo.completed}></input>
+                <label>{todo.todos}</label>
+                <FaRegTrashAlt onClick= {() => HandleRemoveToDo(index , todo.id)}/>
+            </Repository>
 
         ))}
-
-
-
+        <div>
+            <button  onClick= {e => setFilter("all")}>all</button>
+            <button  onClick= {e => setFilter("completed")}>Completed</button>
+            <button  onClick= {e => setFilter("pending")}>Pending</button>
+        </div>
         </Card>
 
     </>
